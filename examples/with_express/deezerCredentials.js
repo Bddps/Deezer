9266804b6fd8310851408762cb68c94303c83736
Daniel <bdps.torres@gmail.com>
2015-07-07 15:46:02 -0500

    Update Procfile

--- examples/with_express/deezerCredentials.js
+++ examples/with_express/deezerCredentials.js
@@ -1,6 +1,6 @@
 module.exports = {
 	id: '161065',
 	secret: '5215360cf25dca3ee4cac43cee0cc5d7',
-	callback: 'http://deezer.herokuapp.com/deezer/callback',
+	callback: 'http://herokuapp.com/deezer/callback',
 	callbackdev: 'http://localhost:5000/deezer/callback'
 };
